2c284c02216aa9e3fc658ba3967db163d085c7c2
Dnl <daniel.souza@eleevait.com.br>
2018-05-06 17:19:20 -0300

    First commit

--- src/main/java/br/com/financeiro/FinanceiroApplication.java
+++ src/main/java/br/com/financeiro/FinanceiroApplication.java
@@ -1,21 +1,12 @@
 package br.com.financeiro;
 
-import br.com.financeiro.dao.LancamentoContabilRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 
-@ComponentScan
-@EnableMongoRepositories
+
 @SpringBootApplication
 public class FinanceiroApplication {
-
-	@Resource
-	private LancamentoContabilRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceiroApplication.class, args);
@@ -24,15 +15,6 @@ public class FinanceiroApplication {
 
 	@PostConstruct
 	public void loadData() {
-//		List<Product> products = new ArrayList<>();
-//		int id = 1;
-//		products.add(new Notebook(id++, "Apple", "MacBook Pro", 15));
-//		products.add(new SmartPhone(id++, "Motorola", "Moto X", 5, true, true));
-//		products.add(new SmartPhone(id++, "Apple", "Iphone 6 Plus", 6, true, true));
-//		products.add(new Pendrive(id++, "Kingston", "Data Traveler ", 16));
-//		products.add(new Pendrive(id++, "Sandisk", "Cruzer Fit", 32));
-//		products.add(new SmartPhone(id++, "Nokia", "6230", 6, true, false));
-//		repository.insert(products);
 	}
 
 }
--- src/main/java/br/com/financeiro/config/MongoConfig.java
+++ src/main/java/br/com/financeiro/config/MongoConfig.java
@@ -8,29 +8,3 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration(exclude = { EmbeddedMongoAutoConfiguration.class })
 public class MongoConfig{
 }
-/*
-
-@RunWith(SpringRunner.class)
-@DataMongoTest
-public class UserRepositoryTests {
-
-    @Autowired
-    UserRepository dao;
-
-    @Before
-    public void setUp() {
-
-        User user = new User();
-
-        user.setName("test");
-        dao.save(user);
-    }
-
-    @Test
-    public void findByName() {
-        List<User> result = dao.findByName("test");
-        assertThat(result).hasSize(1).extracting("name").contains("test");
-    }
-
-}
- */
